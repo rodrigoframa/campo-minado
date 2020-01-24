058aac2f6f2a4496678808b0c6d3afc7bf2d5366
Rodrigo Frazão Maia <rodrigoframa@gmail.com>
2020-01-23 22:40:50 -0300

    feat(Tabuleiro): criacao de construtor e atributos da classe Tabuleiro

---
+++ src/br/com/cod3r/cm/modelo/Tabuleiro.java
@@ -0,0 +1,32 @@
+package br.com.cod3r.cm.modelo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Tabuleiro {
+
+	private int linhas;
+	private int colunas;
+	private int minas;
+	
+	private final List<Campo> campos = new ArrayList<>();
+
+	public Tabuleiro(int linhas, int colunas, int minas) {
+		this.linhas = linhas;
+		this.colunas = colunas;
+		this.minas = minas;
+		
+		gerarCampos();
+		associarVizinhos();
+		sortearMinas();
+	}
+
+    private void associarVizinhos() {
+    }
+
+    private void sortearMinas() {
+    }
+
+    private void gerarCampos() {
+    }
+}
