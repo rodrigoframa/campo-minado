93af5b9c3331e1c06b62292de2114aad9cdd014f
Rodrigo Frazão Maia <rodrigoframa@gmail.com>
2020-01-23 22:38:46 -0300

    feat(Campo): implementado metodo com o resultado do objetivo alcancado

--- src/br/com/cod3r/cm/modelo/Campo.java
+++ src/br/com/cod3r/cm/modelo/Campo.java
@@ -48,9 +48,6 @@ public class Campo {
         }
     }
 
-    public boolean isMarcado() {
-        return marcado;
-    }
 
     boolean abrir() {
 
@@ -75,9 +72,12 @@ public class Campo {
         return vizinhos.stream().noneMatch(v -> v.minado);
     }
 
-
     void minar() {
 	    minado = true;
+    }
+
+    public boolean isMarcado() {
+        return marcado;
     }
 
     public boolean isAberto() {
@@ -86,6 +86,44 @@ public class Campo {
 
     public boolean isFechado() {
         return !isAberto();
+    }
+
+    public int getLinha() {
+        return linha;
+    }
+
+    public int getColuna() {
+        return coluna;
+    }
+
+    boolean objetivoAlcancado() {
+        boolean desvendado = !minado && aberto;
+        boolean protegido = minado && marcado;
+        return desvendado || protegido;
+    }
+
+    long minasNaVizinhanca() {
+        return vizinhos.stream().filter(v -> v.minado).count();
+    }
+
+    void reiniciar() {
+        aberto = false;
+        minado = false;
+        marcado = false;
+    }
+
+    public String toString() {
+        if(marcado) {
+            return "x";
+        } else if(aberto && minado) {
+            return "*";
+        } else if(aberto && minasNaVizinhanca() > 0) {
+            return Long.toString(minasNaVizinhanca());
+        } else if(aberto) {
+            return " ";
+        } else {
+            return "?";
+        }
     }
 
 }
